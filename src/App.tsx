3aaf60131ac63ac5f063d1700a0e458d8e7966c0
hooooooooooooon <douglaskim91@gmail.com>
2024-05-31 16:27:49 +0900

    Header

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './routes/Home';
+import Tv from './routes/Tv';
+import Search from './components/Search';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
